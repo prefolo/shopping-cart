3e3d06f792bba652c368b017cbe81a618afde8ac
prefolo <webintosh@yahoo.it>
2023-07-16 09:44:17 +0200

    refactor product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,16 +4,11 @@ import images from '../images';
 const Product = ({thumbnail,title,description,price,brand,category}) => {
   return ( 
     <div className='product'>
-        <p>{title}</p>
-        <div className="img-description-container">
-            <img src={images[thumbnail]}/>
-            <div className='info-container'>
-                <p>{description}</p>
-                <div className="product-footer-container">
-                    <button className="price">{`${price} €`}</button>
-
-                </div>
-            </div>
+        <p className='title'>{title}</p>
+        <p className='desc'>{description}</p>
+        <img src={images[thumbnail]}/>
+        <div className="button-container">       
+            <button className="price">{`${price} €`}</button>
         </div>
     </div>
    );
